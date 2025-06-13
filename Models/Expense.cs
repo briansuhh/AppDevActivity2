3bbff07e31f0c92455aa19cd3abec7d109d36f15
ayumi <brianmaysebastian@gmail.com>
2025-06-13 15:17:32 +0800

    Add project files.

---
+++ Models/Expense.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class Expense
+    {
+        public int id { get; set; }
+        public decimal value { get; set; }
+        [Required]
+        public string? description { get; set; }
+
+    }
+}
---
+++ Models/ExpensesDBContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Models
+{
+    public class ExpensesDbContext : DbContext
+    {
+        public DbSet<Expense> Expenses { get; set; }
+
+        public ExpensesDbContext(DbContextOptions<ExpensesDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
